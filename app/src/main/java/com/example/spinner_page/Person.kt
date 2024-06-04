7754130dee3bf16a92c7d1d0e684d8528da152e7
Kuru07 <kumarakuru7007@gmail.com>
2024-06-04 19:45:16 +0530

    Prototype commit

--- app/src/main/java/com/example/spinner_page/Person.kt
+++ app/src/main/java/com/example/spinner_page/Person.kt
@@ -6,22 +6,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import kotlin.random.Random
 
-
-val red ="Red";
-val orange ="Orange";
-val green = "Green";
-val yellow = "Yellow";
-val blue ="Blue";
-val purple ="Purple";
-val magneta = "Magneta";
-val indigo = "Indigo";
-
-
 @Composable
 fun threePerson(){
     val randomNumber= (1..3).random()
     Column {
-        Text(text = "3 persons")
         colourChoose(num = randomNumber)
         }
 }
@@ -29,7 +17,6 @@ fun threePerson(){
 fun fourPerson(){
     val randomNumber= (1..4).random()
     Column {
-        Text(text = "4 persons")
         colourChoose(num = randomNumber)
     }
 }
@@ -37,7 +24,6 @@ fun fourPerson(){
 fun fivePerson(){
     val randomNumber= (1..5).random()
     Column {
-        Text(text = "5 persons")
         colourChoose(num = randomNumber)
     }
 }
@@ -45,16 +31,13 @@ fun fivePerson(){
 fun sixPerson(){
     val randomNumber= (1..6).random()
     Column {
-        Text(text = "6 persons")
         colourChoose(num = randomNumber)
     }
 }
 @Composable
 fun sevenPerson(){
     val randomNumber= (1..7).random()
-
     Column {
-        Text(text = "7 persons")
         colourChoose(num = randomNumber)
     }
 }
@@ -62,20 +45,19 @@ fun sevenPerson(){
 fun eightPerson(){
     val randomNumber= (1..8).random()
     Column {
-        Text(text = "8 persons")
         colourChoose(num = randomNumber)
     }
 }
 @Composable
 fun colourChoose(num:Int){
     when(num){
-        1 -> Text(text = "Red must pay the Bill")
-        2 -> Text(text = "Orange must pay the Bill")
+        1 -> Text(text = "Blue must pay the Bill")
+        2 -> Text(text = "Red must pay the Bill")
         3 -> Text(text = "Green must pay the Bill")
-        4 -> Text(text = "Yellow must pay the Bill")
-        5 -> Text(text = "Blue must pay the Bill")
-        6 -> Text(text = "Purple must pay the Bill")
-        7 -> Text(text = "Magneta must pay the Bill")
-        8 -> Text(text = "Indigo must pay the Bill")
+        4 -> Text(text = "Orange must pay the Bill")
+        5 -> Text(text = "Purple must pay the Bill")
+        6 -> Text(text = "Yellow  must pay the Bill")
+        7 -> Text(text = "Pink must pay the Bill")
+        8 -> Text(text = "Gold must pay the Bill")
     }
 }
--- app/src/main/java/com/example/spinner_page/Spinner_Activity.kt
+++ app/src/main/java/com/example/spinner_page/Spinner_Activity.kt
@@ -1,7 +1,12 @@
 package com.example.spinner_page
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Slider
@@ -13,11 +18,15 @@ import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import kotlin.math.roundToInt
 import com.example.spinner_page.whenSlider as whenSlider
+
 
 @Composable
 fun SpinnerActivity(){
@@ -27,30 +36,43 @@ fun SpinnerActivity(){
     var switchClick by remember{
         mutableFloatStateOf(0F)
     }
-    Column (modifier = Modifier.padding(50.dp)){
+    Column (modifier = Modifier
+        .padding(50.dp)
+        .fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally){
+        when(sliderPosition){
+            3F -> Image(painter = painterResource(R.drawable.wheel_3), contentDescription = null, modifier = Modifier.padding(0.dp) )
+            4F -> Image(painter = painterResource(R.drawable.wheel_4), contentDescription = null, modifier = Modifier.padding(0.dp) )
+            5F -> Image(painter = painterResource(R.drawable.wheel_5), contentDescription = null, modifier = Modifier.padding(0.dp) )
+            6F -> Image(painter = painterResource(R.drawable.wheel_6), contentDescription = null, modifier = Modifier.padding(0.dp) )
+            7F -> Image(painter = painterResource(R.drawable.wheel_7), contentDescription = null, modifier = Modifier.padding(0.dp) )
+            8F -> Image(painter = painterResource(R.drawable.wheel_8), contentDescription = null, modifier = Modifier.padding(0.dp) )
+        }
+        Spacer(modifier = Modifier.padding(15.dp))
         Slider(value = sliderPosition,
-            onValueChange ={sliderPosition=it.roundToInt().toFloat()
-                switchClick=0F},
-            colors = SliderDefaults.colors(
-                thumbColor = Color.Green,
-                activeTrackColor = Color.Blue,
-                inactiveTrackColor = Color.Red
-            ),
-            steps = 4,
-            valueRange = 3F..8F
-        )
-        Text(text =sliderPosition.toString() )
+                onValueChange ={sliderPosition=it.roundToInt().toFloat()
+                    switchClick=0F},
+                colors = SliderDefaults.colors(
+                    thumbColor = Color.Green,
+                    activeTrackColor = Color.Blue,
+                    inactiveTrackColor = Color.Red
+                ),
+                steps = 4,
+                valueRange = 3F..8F,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.padding(15.dp))
+            Text(text =sliderPosition.toInt().toString()+" Persons")
 
-        Spacer(modifier = Modifier.padding(10.dp))
-        Button(onClick = { switchClick++}){
-            Text(text = "SPIN")
+            Spacer(modifier = Modifier.padding(15.dp))
+            Button(onClick = { switchClick++}){
+                Text(text = "SPIN")
+            }
+
+            if(switchClick>0){
+                whenSlider(sliderPosition = sliderPosition)
+
+            }
         }
-        if(switchClick>0){
-            whenSlider(sliderPosition = sliderPosition)
-
-        }
-
-    }
 }
 
 @Composable
