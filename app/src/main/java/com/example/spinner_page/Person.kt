dc500505e8e31a3d78a853e6a7b2b807bbd314b4
sudeepanv <vsudeepan@gmail.com>
2024-06-06 01:55:07 +0530

    Random value not changing

--- app/src/main/java/com/example/spinner_page/Person.kt
+++ app/src/main/java/com/example/spinner_page/Person.kt
@@ -1,62 +1,27 @@
 package com.example.spinner_page
 
-import android.graphics.Color
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import kotlin.random.Random
+import androidx.compose.ui.tooling.preview.Preview
+
+
+import androidx.compose.runtime.*
+@Composable
+fun PayBill(numberOfPeople: Int, reset: Boolean = false) : Int {
+    var chosenNumber by remember { mutableIntStateOf((1..numberOfPeople).random()) }
+    if (reset) {
+        chosenNumber = (1..numberOfPeople).random()
+    }
+    Column {
+        colourChoose(chosenNumber)
+    }
+    return chosenNumber
+}
 
 @Composable
-fun threePerson():Int{
-    val randomNumber= (1..3).random()
-    Column {
-        colourChoose(num = randomNumber)
-        }
-    return randomNumber
-}
-@Composable
-fun fourPerson():Int{
-    val randomNumber= (1..4).random()
-    Column {
-        colourChoose(num = randomNumber)
-    }
-    return randomNumber
-}
-@Composable
-fun fivePerson():Int{
-    val randomNumber= (1..5).random()
-    Column {
-        colourChoose(num = randomNumber)
-    }
-    return randomNumber
-}
-@Composable
-fun sixPerson():Int{
-    val randomNumber= (1..6).random()
-    Column {
-        colourChoose(num = randomNumber)
-    }
-    return randomNumber
-}
-@Composable
-fun sevenPerson():Int{
-    val randomNumber= (1..7).random()
-    Column {
-        colourChoose(num = randomNumber)
-    }
-    return randomNumber
-}
-@Composable
-fun eightPerson():Int{
-    val randomNumber= (1..8).random()
-    Column {
-        colourChoose(num = randomNumber)
-    }
-    return randomNumber
-}
-@Composable
-fun colourChoose(num:Int){
-    when(num){
+fun colourChoose(num: Int) {
+    when (num) {
         1 -> Text(text = "Blue must pay the Bill")
         2 -> Text(text = "Red must pay the Bill")
         3 -> Text(text = "Green must pay the Bill")
@@ -66,4 +31,10 @@ fun colourChoose(num:Int){
         7 -> Text(text = "Pink must pay the Bill")
         8 -> Text(text = "Gold must pay the Bill")
     }
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    PayBill(numberOfPeople = 8)
 }
